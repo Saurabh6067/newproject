bed35fec5bdb425e45f0f4f046b3f45afcf1656e
Saurabh6067 <saurabh6067@gmail.com>
2023-12-26 21:51:38 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// take json file with limit
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+//take data through url
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// public asitis 
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8001,()=>{
+        console.log(`Server is running : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
