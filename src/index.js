3231ce747bce3d99d82eab8e69170281eb358f1a
Saurabh6067 <saurabh6067@gmail.com>
2023-12-26 20:38:11 +0530

    connect to mongoatlas

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+// (async()=>{
+//     try {
+//       await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error("ERROR:",error);
+//         throw error
+//     }
+// })()
+
